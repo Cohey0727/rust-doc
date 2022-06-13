a4b5428adfcff5085eca64f18988b45932b5cfa2
Kohei <ohayousagi.ac.kook0727@gmail.com>
2022-06-13 23:49:16 +0900

    main

--- chapter-3/sample/src/main.rs
+++ chapter-3/sample/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    another_function(5);
+
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    let x = plus_one(x);
+    println!("The value of x is: {}", x);
+    if_state();
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {}", x);
+}
+
+fn five() -> i32 {
+    5 // return value
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn if_state() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
