7541bfdff192889470b062d5e5701c2490e06ce0
Kohei <ohayousagi.ac.kook0727@gmail.com>
2022-06-27 13:33:39 +0900

    main

--- 7. Managing Growing Projects with Packages, Crates, and Modules/restaurant/src/lib.rs
+++ 7. Managing Growing Projects with Packages, Crates, and Modules/restaurant/src/lib.rs
@@ -1,24 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+mod front_of_house;
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-}
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
-    // Absolute path
-    // 絶対パス
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // Relative path
-    // 相対パス
-    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
